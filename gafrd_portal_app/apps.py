ab2faa416c50853470448116d0bf8be5f229fd15
Ahmed Kotb <A7madKotb@gmail.com>
2021-11-22 15:35:38 +0200

    Initial commit

---
+++ gafrd_portal_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GafrdPortalAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gafrd_portal_app'
---
+++ gafrd_portal_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+def index(request):
+    """View function for home page of site."""
+
+    # Render the HTML template index.html
+    return render(request, 'index.html')
