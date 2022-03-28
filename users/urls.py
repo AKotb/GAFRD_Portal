7c925b5fdb5048f34abf3cca4081dddf995ee455
Ahmed Kotb <A7madKotb@gmail.com>
2022-03-28 13:11:04 +0200

    Fixed deprecated url usage

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,10 @@
 # users/urls.py
 
-from django.conf.urls import include, url
-
+from django.urls import include, re_path
 from users.views import dashboard, register
 
 urlpatterns = [
-    url(r"^accounts/", include("django.contrib.auth.urls")),
-    url(r"^dashboard/", dashboard, name="dashboard"),
-    url(r"^register/", register, name="register"),
+    re_path(r"^accounts/", include("django.contrib.auth.urls")),
+    re_path(r"^dashboard/", dashboard, name="dashboard"),
+    re_path(r"^register/", register, name="register"),
 ]
