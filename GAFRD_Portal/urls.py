700183e0e010881f614d4d2d797999a34520ea08
Ahmed Kotb <A7madKotb@gmail.com>
2022-03-28 09:56:24 +0200

    Fix deprecated url

--- GAFRD_Portal/urls.py
+++ GAFRD_Portal/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include
-from django.urls import path
+from django.urls import include, path, re_path
 from django.views.generic import RedirectView
 
 urlpatterns = [
@@ -26,6 +24,6 @@ urlpatterns = [
     path('gafrd_portal_app/', include('gafrd_portal_app.urls')),
     path('', RedirectView.as_view(url='gafrd_portal_app/', permanent=True)),
     path(r'^favicon\.ico$', RedirectView.as_view(url='/static/img/favicon.png')),
-    url(r"^", include("users.urls")),
+    re_path(r"^", include("users.urls")),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
